08e14068f28f326571709b55b894609e0c9e5d00
belaymit <2belamit@gmail.com>
2022-12-23 19:58:58 +0300

    added async function that loads fake JSON data

--- src/modules/fetchProduct.js
+++ src/modules/fetchProduct.js
@@ -3,7 +3,7 @@ import displayProduct from './displayProduct.js';
 const fetchProduct = async () => {
   const response = await fetch('https://fakestoreapi.com/products');
   const data = response.json();
-  data.then((object) => displayProduct(object.result));
+  data.then((object) => displayProduct(object));
 };
 
 export default fetchProduct;
