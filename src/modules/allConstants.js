0234dc90e08097fe3fa8f7a5337cb7edd09ad1ed
belaymit <2belamit@gmail.com>
2022-12-24 10:06:46 +0300

    added cartList and cartTotalValue to constants

--- src/modules/allConstants.js
+++ src/modules/allConstants.js
@@ -3,3 +3,6 @@ export const navbarCollapse = document.querySelector('.navbar-collapse');
 export const cartContainer = document.querySelector('.cart-container');
 export const cartBtn = document.querySelector('#cart-btn');
 export const productList = document.querySelector('.product-list');
+export const cartList = document.querySelector('.cart-list');
+export const cartTotalValue = document.getElementById('cart-total-value');
+export const cartCountInfo = document.getElementById('cart-count-info');
